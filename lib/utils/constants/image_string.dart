bdf96149f99835e3a4da3a0aa742067f72e9058d
ARgod7 <krishanarpit2003@gmail.com>
2024-05-26 15:40:15 +0530

    added packages

---
+++ lib/utils/constants/image_string.dart
@@ -0,0 +1,9 @@
+class AImages {
+  // App logos
+  static const String darkAppLogo = 'assets/logos/';
+  static const String lightAppLogo = 'assets/logos/';
+
+  // Social logos
+  static const String google = 'assets/logos/';
+  static const String facbook = 'assets/logos/';
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,19 @@
+class ATexts {
+  // Global Text
+  static const String text1 = '';
+  static const String text2 = '';
+  static const String text3 = '';
+
+  // ONBoarding Text
+  static const String onBoardingText1 = '';
+  static const String onBoardingText2 = '';
+  static const String onBoardingText3 = '';
+
+  static const String onBoardingSubTitleText1 = '';
+  static const String onBoardingSubTitleText2 = '';
+  static const String onBoardingSubTitleText3 = '';
+
+  // Home Text
+  static const String homeAppBarTitle = '';
+  static const String homeAppBarSubTitle = '';
+}
